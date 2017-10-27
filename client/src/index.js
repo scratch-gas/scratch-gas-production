c581abff8d4b21ac38612c043a368e3eb727a8f0
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-10-27 13:57:16 -0400

    Add fully functioning barebones components: App, Dir, File

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDom.render(
-  <App />, 
-  document.getElementById('root')
-);
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Dir from './components/Dir.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Scratch</h1>
+        This is the App Component
+        <Dir />
       </div>
     );
   }
