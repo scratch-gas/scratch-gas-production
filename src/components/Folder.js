014bedf478b667de2e146e06040c3a0e26d1f529
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-10-27 13:50:15 -0400

    Add components folder inside src folder Add new component called Folder

---
+++ src/components/Folder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Folder extends Component {
+  render() {
+    return (
+      <div className="Folder">
+      </div>
+    );
+  }
+}
+
+export default Folder;
