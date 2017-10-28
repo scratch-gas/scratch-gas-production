470ee184a2c63062ee801b1296eabadb1c145b04
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-10-28 15:19:28 -0400

    Add data module that contains a mock object with js file structure and a actual object that will hold the information to query from

---
+++ data.js
@@ -0,0 +1,26 @@
+// this is an example of what the real data will looklike. Using our own file structure
+let mock = {
+  authentication: {
+    files: [ 'config.js', 'oauth.js', 'passport.js', 'serial.js', 'strategies.js', 'testAuth.js' ]
+  },
+  client: {
+    components: {
+      files: [ 'Dir.js', 'File.js', 'Home.js', 'Login.js', 'PrivateRoute.js', 'RublicRepo.js', 'SingleRepo.js']
+    },
+    files: [ 'App.js' ]
+  },
+  controller: {
+    files: [ 'getController.js', 'postController.js' ]
+  },
+  static: {
+    files: [ 'index.html', 'webpack-bundle.js' ]
+  },
+  files: [ 'server.js', 'webpack.config.js', 'package.json', 'package-lock.json', 'data.js' ]
+}
+
+let actual = {}
+
+module.exports = {
+  mock: mock,
+  actual: actual
+}
